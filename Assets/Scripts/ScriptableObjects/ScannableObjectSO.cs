7d83e381875fca826cb2f49d1f77783e6c0a07f9
Raxadore <liam@nellakir.com.au>
2025-08-01 21:52:23 +1000

    ScannableObjectSO
    Created the base script for the scriptable object that will contain the details for any scannable objects.

---
+++ Assets/Scripts/ScriptableObjects/ScannableObjectSO.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScannableObjectSO", menuName = "Scanning/ScannableObjectSO")]
+public class ScannableObjectSO : ScriptableObject
+{
+    //Where all the variables are declared
+    [Header("---- Settings ----")]
+    [SerializeField] private bool hasBeenScanned;
+
+    [Header("---- Details ----")]
+    [SerializeField] private string objectName;
+    [TextArea] [SerializeField] private string objectDesc;
+    [SerializeField] private Sprite objectSprite;
+
+}
