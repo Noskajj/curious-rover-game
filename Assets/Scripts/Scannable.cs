211f5a3fa00e7e0012dad46f89e33e1941e9822d
Raxadore <liam@nellakir.com.au>
2025-08-24 15:04:15 +1000

    Testing Scan Mechanics
    Tried adding scanning focusing on the object you are trying to scan.

--- Assets/Scripts/Scannable.cs
+++ Assets/Scripts/Scannable.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -17,55 +18,99 @@ public class Scannable : MonoBehaviour
     [SerializeField]
     private GameObject thisObject;
 
+    [SerializeField]
+    private GameObject sceneCamera;
+
+    private CinemachineCamera cinCamera;
+
+    private CinemachineRotationComposer camRotation;
+
+    private CinemachineOrbitalFollow camOrbit;
+
     private Material successMat;
 
     private GameObject scanTarget;
 
     public static bool overObject = false;
 
-    private InputSystem_Actions inputActions;
+    private InputAction scanButton;
 
-    private float timeToScan = 3f;
+    private float timeToScan = 3f, pressStartTime;
+
+    private bool isPressed;
+
+    private Vector3 mainCamOffset, scanningOffset;
+
+    private float rigTop, rigMid, rigBot;
 
     private void Start()
     {
+        cinCamera = sceneCamera.GetComponent<CinemachineCamera>();
+        camRotation = sceneCamera.GetComponent<CinemachineRotationComposer>();
+        camOrbit = sceneCamera.GetComponent<CinemachineOrbitalFollow>();
+
+        mainCamOffset = camRotation.TargetOffset;
+        scanningOffset = new Vector3(0,0,0);
         TestScan();
-        inputActions = new InputSystem_Actions();
-        inputActions.Enable();
-        inputActions.Player.Scan.performed += context => Scan();
+        scanButton = InputSystem.actions.FindAction("Scan");
+
+        scanButton.started += Scan;
+        scanButton.canceled += ScanFailed;
+        
+        camOrbit.enabled = true;
+        
+        rigTop = camOrbit.Orbits.Top.Radius;
+        rigMid = camOrbit.Orbits.Center.Radius;
+        rigBot = camOrbit.Orbits.Bottom.Radius;
     }
 
     private void Update()
     {
-        /*CONSIDER CHANGING IT TO ONE CURSOR OBJECT AND 
-         CHANGE THE VISUALS WITH ANIMATOR TO HELP
-         SIMPLIFY THE CODE
-        */
+        //UpdateCursor();
 
-        /*if(overObject)
+        if (isPressed && Time.time - pressStartTime >= timeToScan)
         {
-            regularCursor.SetActive(false);
-            scanningCursor.SetActive(true);
+            ScanSuccessful();
         }
-        else
+        else if (isPressed)
         {
-            regularCursor.SetActive(true);
-            scanningCursor.SetActive(false);
-        }*/
-
-        
+            Debug.Log(Time.time - pressStartTime);
+        }
     }
 
-    private void Scan()
+    private void Scan(InputAction.CallbackContext context)
     {
-        //Need to lock on to object with camera when scanning
-        //Need to set a time holding E to scan
         if (scanTarget != null)
         {
-            Debug.Log("You Scanned: " + scanTarget.name);
-            scanTarget.transform.GetComponent<MeshRenderer>().material = successMat;
-            scanTarget.GetComponent<ScannableObject>().SuccessfullyScanned();
+            Debug.Log("Started scanning");
+            isPressed = true;
+            pressStartTime = Time.time;
+
+            cinCamera.LookAt = scanTarget.transform;
+            camRotation.TargetOffset = scanningOffset;
+            SetRigRadii(1, 1, 1);
         }
+    }
+
+    private void ScanFailed(InputAction.CallbackContext context)
+    {
+        isPressed = false;
+        cinCamera.LookAt = thisObject.transform;
+        camRotation.TargetOffset = mainCamOffset;
+        SetRigRadii(rigTop, rigMid, rigBot);
+    }
+
+    private void ScanSuccessful()
+    {
+        isPressed = false;
+        scanTarget.GetComponent<ScannableObject>().SuccessfullyScanned();
+        cinCamera.LookAt = thisObject.transform;
+        camRotation.TargetOffset = mainCamOffset;
+        SetRigRadii(rigTop, rigMid, rigBot);
+
+        Debug.Log("You Scanned: " + scanTarget.name);
+        scanTarget.transform.GetComponent<MeshRenderer>().material = successMat;
+        scanTarget.GetComponent<ScannableObject>().SuccessfullyScanned();
     }
 
     private void TestScan()
@@ -83,5 +128,31 @@ public class Scannable : MonoBehaviour
     public void SetScanTarget(GameObject scanTarget)
     {
         this.scanTarget = scanTarget;
+    }
+
+    private void UpdateCursor()
+    {
+        /*CONSIDER CHANGING IT TO ONE CURSOR OBJECT AND 
+         CHANGE THE VISUALS WITH ANIMATOR TO HELP
+         SIMPLIFY THE CODE
+        */
+
+        if(overObject)
+        {
+            regularCursor.SetActive(false);
+            scanningCursor.SetActive(true);
+        }
+        else
+        {
+            regularCursor.SetActive(true);
+            scanningCursor.SetActive(false);
+        }
+    }
+
+    private void SetRigRadii(float top, float mid, float bot)
+    {
+        camOrbit.Orbits.Top.Radius = top;
+        camOrbit.Orbits.Center.Radius = mid;
+        camOrbit.Orbits.Bottom.Radius = bot;
     }
 }
