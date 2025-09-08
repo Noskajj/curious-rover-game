1d3687f37fa89e8aec01f430845e428822701ed7
Raxadore <31946832+Raxadore@users.noreply.github.com>
2025-09-08 10:43:30 +1000

    Trying to fix merge issues
    Edited the git files to try and use UnityYamlMerge.

---
+++ Assets/Scripts/Checklist.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Checklist
+{
+    /*
+     * Fix Gravity
+     * Fix Camera not returning to right position for main camera
+     * Set fps cam to face forward when switching away from it
+     * Turn off character model when switching to fps cam(maybe?)
+     * Only scan in scanning mode
+     * Consider turning off highlighting when not in camera mode
+     * Create database pop up when scanning something
+     */
+}
